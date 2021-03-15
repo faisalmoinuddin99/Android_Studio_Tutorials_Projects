4d32eb3424d842cbb30d333ee281676b4d99acf0
faisal <faisal25march99@gmail.com>
2021-03-15 05:43:29 +0530

    Done with Problem 3 - Multiplication App

--- Chapter1- Practice set/app/src/main/java/com/example/greeting/MainActivity.java
+++ Chapter1- Practice set/app/src/main/java/com/example/greeting/MainActivity.java
@@ -16,29 +16,43 @@ public class MainActivity extends AppCompatActivity {
     EditText editText1 ;
     EditText editText2 ;
     Button button ;
+    Button button2 ;
     TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
         editText1  = findViewById(R.id.num1);
         editText2 = findViewById(R.id.num2) ;
         textView = findViewById(R.id.text) ;
+
+
+
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Add Button Clicked", Toast.LENGTH_SHORT).show();
-               String num1 =  editText1.getText().toString() ;
-               String num2 = editText2.getText().toString() ;
 
-               int S_num1 =Integer.parseInt(num1) ;
-               int S_num2 = Integer.parseInt(num2) ;
 
-               int result = S_num1 + S_num2 ;
-               textView.setText("Addition of " + num1  +" and " + num2  + " =" + result);
+               // Addition
+                int sum = Integer.parseInt(editText1.getText().toString()) + Integer.parseInt(editText2.getText().toString());
+                textView.setText("The Sum is " + sum);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Multiplication
+                int mul = Integer.parseInt(editText1.getText().toString()) * Integer.parseInt(editText2.getText().toString());
+                textView.setText("The Multiplication is " + mul);
             }
         });
 
